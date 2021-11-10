e0266231692010b34446f40e8e0819f3c27fb93c
trapq3dU <49921218+trapq3dU@users.noreply.github.com>
2021-11-10 10:08:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+
+document.getElementById("createTodo").focus();
+
+function onSubmit (){
+    const newTodo = document.getElementById("createTodo").value;
+    const str = newTodo.charAt(0).toUpperCase() + newTodo.slice(1);
+    
+    if(str.length >= 1) {
+        showTodo(str);
+        clear()
+    }
+    else {
+        alert("You can't add empty todo")
+        return
+    }
+    return    
+}
+
+var divId = 0;
+function showTodo(str){
+
+    const section = document.getElementById("showTodo")
+
+    const div = document.createElement("div")
+    div.id = divId;
+    div.className = "todo"
+    section.appendChild(div);
+
+    const h3 = document.createElement('h3')
+    
+    h3.innerText = str;
+    div.appendChild(h3);
+
+    
+    const btn = document.createElement('button')
+    btn.setAttribute(`onclick`,`removeTodo(${divId})`)
+    btn.className = "deleteBtn"
+    btn.innerText = `X`
+    div.appendChild(btn)
+    divId++
+    return
+
+}
+
+function clear(){
+    
+    document.getElementById("createTodo").value = "";
+    document.getElementById("createTodo").focus();
+    return;
+}
+
+
+function removeTodo (id) {
+    document.getElementById(id).remove();
+    document.getElementById("createTodo").focus();
+    return
+    
+}
